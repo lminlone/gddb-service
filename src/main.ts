3ac90e02aba43f398e08b6c1369ba23b331fb433
WiggleWizard <1405402+WiggleWizard@users.noreply.github.com>
2024-05-14 09:37:43 +0100

    Some more debugging bs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ console.log("Starting...");
 import { logger } from './logger';
 logger.info("Application spinning up");
 
+console.log("Main logger should be initialized");
+
 import { Server } from './server';
 var server: Server = Server.getInstance();
 server.initialize();
